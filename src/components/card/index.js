58409a701d33c3a34d9f916e6982625f92df4eb6
Guilherme Davi <guiionfire@hotmail.com>
2019-06-06 14:31:57 -0300

    titleCase function added

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './card.css';
 
+const titleCase = (name) => {
+  return name[0].toUpperCase() + name.slice(1);
+};
+
 const Card = ({ pokemon }) => {
   return (
     <div className="card">
-      <p className="name">{pokemon.name}</p>
+      <p className="name">{titleCase(pokemon.name)}</p>
       <p className="date">17-08-2017</p>
     </div>
   );
